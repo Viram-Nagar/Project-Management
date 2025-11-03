c9b8ee99cb6dc9f3535c7d03f32fd3b7e78a78c7
Viram Nagar <viramnagar1019@gmail.com>
2025-11-03 09:14:58 +0530

    api / remove

--- frontend/src/api/projectAPI.jsx
+++ frontend/src/api/projectAPI.jsx
@@ -1,7 +1,7 @@
 import API from "./axios";
 
-export const getProjects = () => API.get("/projects");
-export const getProjectById = (id) => API.get(`/projects/${id}`);
+export const getProjects = () => API.get("projects");
+export const getProjectById = (id) => API.get(`projects/${id}`);
 export const updateProject = (id, data) => API.put(`projects/${id}`, data);
-export const createProject = (data) => API.post("/projects", data);
-export const deleteProject = (id) => API.delete(`/projects/${id}`);
+export const createProject = (data) => API.post("projects", data);
+export const deleteProject = (id) => API.delete(`projects/${id}`);
